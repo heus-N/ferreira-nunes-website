0830a5d2e28a6357ca5021c12b6c87f27be86b71
heus <1matheusnuness@gmail.com>
2024-07-09 23:12:22 -0300

    feat: add transition onClick & onClose images

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide} from 'swiper/react'
 import '../App.css'; // Adicione aqui o CSS para estilizar o modal
 
 const ImageModal = ({ onClose, initialIndex, images = [] }) => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true)
+    }, 10)
+  }, [])
+
+  const handleClose = () => {
+    setIsVisible(false);
+    setTimeout(() => {
+      onClose()
+    }, 300)
+  }
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget){
+      handleClose();
+    }
+  }
+
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-      <button className="close-button" onClick={onClose}>X</button>
+    <div className={`modal-overlay ${isVisible ? 'show' : ''}`} onClick={handleOverlayClick}>
+      <div className={`modal-content ${isVisible ? 'show' : ''}`} onClick={(e) => e.stopPropagation()}>
+      <button className="close-button" onClick={handleClose}>X</button>
         <Swiper initialSlide={initialIndex} navigation pagination={{ clickable: true }}>
           {Array.isArray(images) ? images.map((imageSrc, index) => (
             <SwiperSlide key={index}>
